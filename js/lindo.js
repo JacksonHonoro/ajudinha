b09e68218e56f9633d98d7af55bc5f541f748526
duspontes <robson.tecnologia@gmail.com>
2018-10-10 14:15:53 -0300

    Cabecalho arrumado

--- js/lindo.js
+++ js/lindo.js
@@ -1,5 +1,14 @@
 window.onload = function() {
-	document.body.style.backgroundColor = "blue";
-	document.getElementsByTagName("span")[0].style.color = "red";
-	document.getElementById("titulo").style.color ="red"
+	document.body.style.backgroundColor = "#f0f8ff";
+	document.body.style.margin = "0px";
+	document.body.style.color = "#111";
+	document.body.style.fontFamily = "monospace";	
+	document.getElementsByTagName("h1")[0].style.color = "#f9f9f9";
+	document.getElementsByTagName("h1")[0].style.backgroundColor = "#222";
+	document.getElementsByTagName("h1")[0].style.paddingLeft = "10px";
+
+	y = document.getElementsByTagName("span");		
+	for (i = 0; i < y.length; i++) {
+		y[i].style.marginLeft = "40px";		
+	}	
 };
--- lindo.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	document.body.style.backgroundColor = "blue";
-	document.body.style.color = "white";
-	document.getElementsByTagName("span")[0].style.color = "red";
-	document.getElementById("titulo").style.color ="red"
-};
