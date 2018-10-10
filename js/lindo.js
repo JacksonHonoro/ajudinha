fd43a9014284a54a31121ef15a26bab36bd9f33c
duspontes <robson.tecnologia@gmail.com>
2018-10-10 09:44:45 -0300

    Ajundinha JS

---
+++ js/lindo.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	document.body.style.backgroundColor = "blue";
+	document.getElementsByTagName("span")[0].style.color = "red";
+	document.getElementById("titulo").style.color ="red"
+};
