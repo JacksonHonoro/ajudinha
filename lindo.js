e98ee95158d8c6515aaa3c9e33dfdad394102fbf
duspontes <robson.tecnologia@gmail.com>
2018-10-10 14:11:18 -0300

    Cabecalho

---
+++ lindo.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	document.body.style.backgroundColor = "blue";
+	document.body.style.color = "white";
+	document.getElementsByTagName("span")[0].style.color = "red";
+	document.getElementById("titulo").style.color ="red"
+};
